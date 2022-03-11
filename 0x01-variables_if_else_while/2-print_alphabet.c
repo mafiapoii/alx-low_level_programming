85659ceaa17c81433fe1bf0dd9d786dae0224ed3
mafiaboy <mafiyapoii@gmail.com>
2022-03-11 11:57:34 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return:0
  */
 int main(void)
 {
-	signed char x;
-	int n;
+  char a;
 
-	for (n = 97; n <= 122; n++)
-	{
-	x = n;
-	printf("%c", x);
-	if (n == 122)
-	{
-	printf("\n");
-	}
-	}
-	return (0);
+  for (a = 'a'; a <= 'z' ; a++)
+    putchar(a);
+  putchar('\n');
+  return (0);
 }
