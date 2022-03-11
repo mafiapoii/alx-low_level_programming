74b26618694cc1340c27256ae8e474634fe693e8
mafiaboy <mafiyapoii@gmail.com>
2022-03-11 12:09:52 -0800

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-#include<stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+#include <stdio.h>
+
+/*
+ *main - entry point
+ *return: 0
  */
 int main(void)
 {
-	int n;
-	int lastDigit;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
-	if (lastDigit > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	}
-	if (lastDigit == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	}
-	if (lastDigit != 0 && lastDigit < 6)
-	{
-		puts -e ("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-	}
-	return (0);
+  int n;
+  int lastn;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  lastn = n % 10;
+  if (lastn > 5)
+    {
+      printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+    }
+  else if (lastn == 0)
+    {
+      printf("Last digit of %d is %d and is 0\n", n, lastn);
+    }
+  else
+    {
+      printf("Last digit of %d is %d and is greater than 6 and not 0\n", n, lastn);
+    }
+  return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 10; i < 20; i++)
-    {
-      putchar((i % 10) + '0');
-      if (i != 19)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+        int i;
+        for (i = 10; i < 20; i++)
+        {
+                putchar((i % 10) + '0');
+                if (i != 19)
+          	{
+	                putchar(',');
+	                putchar(' ');                                         
+            	}
+        }
+        putchar('\n'); 
+        return (0);
 }
