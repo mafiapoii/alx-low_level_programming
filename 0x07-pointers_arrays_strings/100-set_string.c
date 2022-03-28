4c89cc66ee602b5e6f03cda5fa51c4edd1f8aad1
mafiaboy <mafiyapoii@gmail.com>
2022-03-28 12:24:10 -0700

    0x07-pointers_arrays_strings/

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,6 @@
 #include "main.h"
 
-/**
-* set_string - sets the value of a pointer to a char
-* @s: pointer
-* @to: pointer
-* Return: 0
-*/
-
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
